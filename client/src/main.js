7a85bb1f6a539d24d89a1c7023dd348765ec24ce
nino <nino.mauuary@gmail.com>
2025-02-24 16:46:58 +0100

    Build:  Install PrimeVue and make general config change to dev dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,47 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
-const app = createApp(App)
+// *** Import primevue components ***
 
-app.use(router)
+import Button from "primevue/button";
 
-app.mount('#app')
+// *** Custom style for primevue ***
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: { // Change primary and secondary colors variable provided by primevue
+            50:  '{pink.5hsl(0, 0%, 0%)0}',
+            100: '{pink.100}',
+            200: '{pink.200}',
+            300: '{pink.300}',
+            400: '#FF92CB',
+            500: '#FF92CB'
+        },
+        secondary: {
+            50:  '{indigo.300}',
+            100: '{indigo.400}',
+            200: '{indigo.500}',
+            300: '{indigo.600}',
+            400: '##5250A4',
+            500: '#5250A4'
+        }
+    }
+});
+
+const app = createApp(App);
+app.component('PrButton', Button);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset
+    }
+});
+
+app.mount('#app');
